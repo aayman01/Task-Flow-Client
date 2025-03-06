1b18064feb14802ca9c1bfdc8fe46f3cae4b180b
aayman01 <ayman.alfi098@gmail.com>
2025-03-06 13:59:49 +0600

    tailwndcss and shadcn setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <div>
       <h2 className='text-5xl font-bold text-red-500'>this is my react app</h2>
+      <Button>Click me</Button>
     </div>
   );
 };
