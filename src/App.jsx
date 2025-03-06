dab9a3de2db1c7ac954512ce54e8d17543b62c7c
aayman01 <ayman.alfi098@gmail.com>
2025-03-06 13:51:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-5xl font-bold text-red-500'>this is my react app</h2>
+    </div>
+  );
+};
+
+export default App;
